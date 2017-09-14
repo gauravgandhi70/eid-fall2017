66996be2c62e6d9f0534f5fb585911f44867eea6
gauravgandhi70 <gaurav.gandhi@colorado.edu>
2017-09-13 22:29:46 -0600

    Changed dog trick and Included name in Readme

--- example.py
+++ example.py
@@ -25,7 +25,7 @@ __copyright__ = "Copyright (C) 2017 by Bruce Montgomery and Bhallaji Venkatesank
 # Redistribution, modification or use of this software in source or binary
 # forms is permitted as long as the files maintain this copyright. Users are
 # permitted to modify this and use it to learn about the field of embedded
-# software. Bruce Montgomery, Bhallaji Venkatesankulathusundaram, and the University of Colorado
+# software. Gaurav Gandhi, and the University of Colorado
 # are not liable for any misuse of this material.
 #
 
@@ -45,7 +45,7 @@ class Dog(object):
 def walker(name):
     """walker: an example of a python function"""
     dog1 = Dog(name)
-    dog1.add_trick("Roll Over")
+    dog1.add_trick("Catch the Ball")
     print("Walking a new dog:")
     print("  The kind of this thing is ",dog1.kind)
     print("  This dog is ",dog1.name)
